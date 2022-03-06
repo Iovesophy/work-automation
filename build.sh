fd9b71f0222496ec7caab7f1062a739150d436fb
Iovesophy <kazu825@me.com>
2022-03-06 21:56:44 +0900

    DEL: rm do-login cmd

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 if [ "$(uname)" = 'Darwin' ]; then
     go build -o genkey cmd/genkey/main.go
-    go build -o do-login cmd/do-login/main.go
     go build -o attach cmd/attach/main.go
     go build -o detach cmd/detach/main.go
     go build -o manhours cmd/manhours/main.go
